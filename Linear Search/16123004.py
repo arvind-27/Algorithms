9a2aea94e2efc47b7eefffce2e8ffda5fda74138
arvind-27 <arvind.agrawal.cse19@itbhu.ac.in>
2020-10-20 14:34:55 +0530

    i have written the code for linear change in python

--- Linear Search/16123004.py
+++ Linear Search/16123004.py
@@ -1,8 +1,13 @@
 k = 4
 l = [ 2,3,4,5,6,8]
-
+flag=0
 n = len(l)
-for i in n:
-	# TODO Fill Code here
+for i in range(n):
+    if l[i] == k:
+        flag=1
+        print("found")
+        break
+if(flag==0):
+    print("the number is not in the list")
 
 
